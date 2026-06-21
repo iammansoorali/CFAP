e771816aa66b27ae31e918dd6a87367319af8cf1
SHAIK MANSOOR ALI <skmakp360@gmail.com>
2026-06-21 12:40:31 +0530

    Create ProfileBadges.tsx

---
+++ apps/web/src/components/ProfileBadges.tsx
@@ -0,0 +1,9 @@
+export default function ProfileBadges() {
+  return (
+    <div>
+      <div>🏅 Eco Warrior</div>
+      <div>🌿 Planet Protector</div>
+      <div>🔥 Consistency Champion</div>
+    </div>
+  );
+}
